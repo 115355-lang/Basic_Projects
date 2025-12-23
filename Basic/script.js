e6a682ce125146a8df95c53c6cff75328ea4d4a3
115355-lang <115355@gcslahore.edu.pk>
2025-12-23 20:41:52 +0500

    Add animated step progress bar component
    Introduced a new animated step progress bar using HTML, CSS, and JavaScript. The component visually represents order progress with interactive steps and navigation buttons, styled for responsiveness and user interaction.

---
+++ Basic/script.js
@@ -0,0 +1,73 @@
+const icon1 = document.querySelector(".icon1");
+const icon2 = document.querySelector(".icon2");
+const icon3 = document.querySelector(".icon3");
+const icon4 = document.querySelector(".icon4");
+const icon5 = document.querySelector(".icon5");
+const next = document.querySelector(".nextbtn");
+const prev = document.querySelector(".prevbtn");
+const reset = document.querySelector(".reset");
+const line=document.querySelectorAll(".div");
+const max = 5;
+const current = 1;
+const
+
+next.addEventListener("click",()=>{
+
+})
+
+icon2.addEventListener("click", () => {
+line[0].classList.add("icons");
+icon2.classList.remove("fa-2");
+icon2.classList.remove("fa-sharp");
+
+icon2.classList.add("fa-solid");
+icon2.classList.add("fa-check");
+icon2.classList.add("icons");
+
+
+// icon1.
+
+})
+icon3.addEventListener("click", () => {
+line[1].classList.add("icons");
+
+icon3.classList.remove("fa-2");
+icon3.classList.remove("fa-sharp");
+
+icon3.classList.add("fa-solid");
+icon3.classList.add("fa-check");
+icon3.classList.add("icons");
+
+// icon1.
+
+})
+
+icon4.addEventListener("click", () => {
+line[2].classList.add("icons");
+
+icon4.classList.remove("fa-2");
+icon4.classList.remove("fa-sharp");
+
+icon4.classList.add("fa-solid");
+icon4.classList.add("fa-check");
+icon4.classList.add("icons")
+
+// icon1.
+
+})
+
+icon5.addEventListener("click", () => {
+line[3].classList.add("icons");
+
+icon5.classList.remove("fa-2");
+icon5.classList.remove("fa-sharp");
+
+icon5.classList.add("fa-solid");
+icon5.classList.add("fa-check");
+icon5.classList.add("icons")
+
+// icon1.
+
+})
+
+
